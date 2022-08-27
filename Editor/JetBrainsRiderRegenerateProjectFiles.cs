6989f160be898885172acd92ce4e89a18b6f8bfe
baba-s <shotababa1206@gmail.com>
2022-08-27 13:21:14 +0900

    add

---
+++ Editor/JetBrainsRiderRegenerateProjectFiles.cs
@@ -0,0 +1,20 @@
+﻿using Packages.Rider.Editor;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+    internal static class JetBrainsRiderRegenerateProjectFiles
+    {
+        [MenuItem( "Kogane/Rider/Regenerate project files" )]
+        private static void SyncSolution()
+        {
+            RiderScriptEditor.SyncSolution();
+        }
+
+        [MenuItem( "Kogane/Rider/Regenerate project files and Open" )]
+        private static void SyncSolutionAndOpenExternalEditor()
+        {
+            RiderScriptEditor.SyncSolutionAndOpenExternalEditor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.JetBrainsRiderRegenerateProjectFiles
+# Kogane JetBrains Rider Regenerate Project Files
+
+Rider の Regenerate project files を実行する MenuItem を追加するエディタ拡張
